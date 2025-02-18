e03d209b1007e266605785f2fff115fab74d63ac
artur.almeida <artur.henrique@defensoria.def.mg.br>
2025-02-18 16:29:07 -0300

    chore/ajuste retorno services users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,62 +1,34 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User, Car } from '@prisma/client';
 import { CreateUserDto } from './dto/create-store.dto';
 import { UpdateUserDto } from './dto/update-store.dto';
-import { ResponseFormat } from 'src/common/types/response-format-type';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) { }
 
-  async findAll(): Promise<ResponseFormat<(User & { cars: Car[] })[]>> {
-    const dados = await this.prisma.user.findMany({
+  findAll(): Promise<(User & { cars: Car[] })[]> {
+    return this.prisma.user.findMany({
       include: {
         cars: true,
       }
     })
-
-    return {
-      dados,
-      mensagem: "Psiuu! Acabamos de retornar os registros para você! ;)"
-    }
-
   }
 
-  async findOne(id: number): Promise<User | null> {
-    const user = await this.prisma.user.findUnique({ where: { id }, include: { cars: true } });
-
-    if (!user) {
-      throw new NotFoundException(`Usuário não encontrado`);
-    }
-
-    return user;
+  findOne(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id }, include: { cars: true } });
   }
 
-  async create(CreateUserDto: CreateUserDto): Promise<ResponseFormat<User>> {
-    const dados = await this.prisma.user.create({ data: { ...CreateUserDto } });
-
-    return {
-      dados,
-      mensagem: 'Tudo certo! Registro criado com sucesso!'
-    }
+  create(CreateUserDto: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({ data: { ...CreateUserDto } });
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto): Promise<ResponseFormat<User>> {
-    const dados = await this.prisma.user.update({ where: { id }, data: { ...updateUserDto } });
-
-    return {
-      dados,
-      mensagem: "Tudo certo! Registro alterado com sucesso!"
-    }
-
+  update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return this.prisma.user.update({ where: { id }, data: { ...updateUserDto } });
   }
 
-  async delete(id: number): Promise<ResponseFormat<boolean>> {
-    await this.prisma.user.delete({ where: { id } });
-    return {
-      dados: true,
-      mensagem: "Tudo certo! Registro removido com sucesso!"
-    }
+  delete(id: number): Promise<User> {
+    return this.prisma.user.delete({ where: { id } });
   }
 }
