d0a9427c00686d1ec15d0377e67fc03133c1148c
artur.almeida <artur.henrique@defensoria.def.mg.br>
2025-02-18 17:47:37 -0300

    feat/ajuste exception

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -7,7 +7,7 @@ import {
 import { Response } from 'express';
 import { Prisma } from '@prisma/client';
 
-@Catch()
+@Catch(Prisma.PrismaClientKnownRequestError, Prisma.PrismaClientUnknownRequestError)
 export class AllExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
@@ -19,8 +19,14 @@ export class AllExceptionFilter implements ExceptionFilter {
   }
 
   private getExceptionDetails(exception: unknown): { status: number; message: string } {
-   
     if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+
+      if (exception.code === 'P2025') {
+        return {
+          status: HttpStatus.NOT_FOUND, 
+          message: 'Registro não encontrado.',
+        };
+      }
       return {
         status: HttpStatus.BAD_REQUEST,
         message: this.mapPrismaError(exception),
@@ -44,7 +50,6 @@ export class AllExceptionFilter implements ExceptionFilter {
     const errorMessages: Record<string, string> = {
       P2002: 'Já existe um registro com esses dados.',
       P2003: 'Referência inválida para outro registro.',
-      P2025: 'Registro não encontrado.',
     };
 
     return errorMessages[exception.code] || 'Erro desconhecido';
